ba46d2e3a7436d4c6223d5de52b88116c7b106d9
MOO GAI CHUNG <272225660@qq.com>
2026-04-09 23:36:19 +0800

    feat.增加了留言板功能

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
-/// <reference types="astro/client" />
+﻿/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_TWIKOO_ENV_ID?: string;
+  readonly PUBLIC_TWIKOO_JS_URL?: string;
+  readonly PUBLIC_TWIKOO_LANG?: string;
+  readonly PUBLIC_TWIKOO_REGION?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
